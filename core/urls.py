b39b28a6595b8feccba848defde6a64e0cf21bcb
kamil27272727 <kamilish27@gmail.com>
2024-08-06 14:33:23 +0600

    Страница с товарами

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('apps.urls')),
+    path('', include('apps.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
